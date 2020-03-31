ac9947aab1deba65e40c4ca12da915a87618146b
xoaquin <xoaquin@Xoaquin-CB>
2020-03-31 22:13:32 +0100

    Added some debugging logs.

--- src/main/java/me/jakegore/orebreakplugin/BlockBreakListener.java
+++ src/main/java/me/jakegore/orebreakplugin/BlockBreakListener.java
@@ -7,19 +7,23 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class BlockBreakListener implements Listener {
+public class BlockBreakListener extends JavaPlugin implements Listener {
     
     @EventHandler
     public void onBreak(BlockBreakEvent event) {
+        getLogger().info("Code 0");
         
         Player player = event.getPlayer();
         ItemStack item = player.getInventory().getItemInMainHand();
         
         if (item.equals(new ItemStack(Material.GOLDEN_PICKAXE))) {
+            getLogger().info("Code 1");
             Block block = event.getBlock();
             
             if (block.getType().equals(Material.COAL_ORE)) {
+                getLogger().info("Code 2");
                 player.giveExpLevels(100);
             }
             

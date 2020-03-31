3080a0ff24e6bad7b72242f166e1e71bd191ea38
Jake Gore <jake.chromebook02@gmail.com>
2020-03-31 19:40:06 +0100

    Initial Commit

---
+++ src/main/java/me/jakegore/orebreakplugin/OreBreakPlugin.java
@@ -0,0 +1,14 @@
+package me.jakegore.orebreakplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class OreBreakPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        getLogger().info("Ore Break Plugin Loaded");
+
+    }
+
+}
